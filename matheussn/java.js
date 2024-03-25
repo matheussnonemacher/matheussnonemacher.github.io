3fa6c9086cf6e5aa6a60d6bc6b43384d3fd782cf
matheussnonemacher <131311516+matheussnonemacher@users.noreply.github.com>
2024-03-25 20:06:29 -0300

    Add files via upload

---
+++ matheussn/java.js
@@ -0,0 +1,56 @@
+/*texto = 'olá meu nome é matheus'
+
+document.write('Texto: '+texto)
+document.write('<br>Tamanho: '+texto.length)
+document.write('<br>Primeira letra: '+texto[0])
+document.write('<br>Última letra: '+texto[texto.length-1]+'<br>')
+
+nome1 = 'Matheus'.toUpperCase()
+nome2 = 'JONAS'.toUpperCase()
+
+palavras = texto.split(' ')
+
+for(i=0; i<6; i+=2){
+document.write('oi '+i+'<br>')
+
+}
+
+texto = 'olá mundo!'
+
+for(i=0; i<texto.length; i++){
+    document.write(texto[i]+'<br>')
+    
+    }*/
+
+    frutas - ['banana', 'uva', 'maçã']
+
+    frutas.push('morango')
+    //frutas[7] = 'pêra'
+
+    //frutas.pop()
+    //frutas.shift()
+    //frutas.splice(2,1)
+    //frutas.splice(frutas.indexOf('uva'),1)
+
+    //frutas.sort()
+    //frutas.reverse()
+
+    //frutas.sort().reverse()
+
+
+
+
+
+    document.write('lista completa: '+frutas)
+    document.write('<br>posiçâo especifica: '+frutas[0])
+    document.write('<br>fruta especifica: '+frutas.indexOf[uva])
+    document.write('<br>tamanho da lista: '+frutas.length)
+
+
+
+
+
+
+    // numeros aleatorios
+
+    alert(Math.floor(Math.random() * 6 + 1))
