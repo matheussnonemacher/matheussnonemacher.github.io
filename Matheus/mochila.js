0f3a1ab7c93f53c85d6a1a26cca8f96662f88084
matheussnonemacher <131311516+matheussnonemacher@users.noreply.github.com>
2024-04-22 19:53:14 -0300

    Add files via upload

--- Matheus/mochila.js
+++ Matheus/mochila.js
@@ -4,8 +4,10 @@ mochila = []
         document.getElementById('capacidade').innerHTML = mochila.length+'/10'
         html = ""
         for(i=0; i<mochila.length; i++){
-            html += '<div>'+mochila[i]+'<button onclick="removerItem('+i+')">X</button></div>'
+            html += '<div><div id="item'+i+'">'+mochila[i]+'</div><button onclick="EditarItem('+i+')">E</button><button onclick="removerItem('+i+')">X</button></div>'
         }
+
+        
         document.getElementById('item').value=""
         document.getElementById('itens').innerHTML = html
     }
@@ -40,4 +42,21 @@ function add(){
 function removerItem(i){
     mochila.splice(i,1)
     atualizarLista()
+}
+
+
+function EditarItem(i){
+atualizarLista()
+
+if(document.getElementById('item'+i).innerHTML == mochila[i]){
+
+    document.getElementById('item' +i).innerHTML = '<input id="editado" type="text" value="'+mochila[i]+'">'
+
+}else{
+
+    mochila[i] = document.getElementById('editado').value
+    atualizarLista()
+}
+
+
 }
